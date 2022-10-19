ae8561e9cd3292422eec3b2fc6b7f8c3c8294131
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-10-19 11:31:09 +0200

    En mapp Services med två klasser added: En FileManager klass för att hantera alla logik i string format.

---
+++ AddressBook/Services/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Services
+{
+    // funktioner som behövs för att läsa och skriva
+    internal interface IfileManager
+    {
+        public void Save(string filePath, string content);
+        public string Read(string filePath);
+    }
+    internal class FileManager : IfileManager // implementering av interfacet
+    {
+        public string Read(string filePath)
+        {
+            using var sr = new StreamReader(filePath);
+            return sr.ReadToEnd();
+        }
+
+        public void Save(string filePath, string content)
+        {
+            using var sw = new StreamWriter(filePath);
+            sw.Write(content);
+        }
+    }
+}
