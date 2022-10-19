56e8b5af1769357cf9125a344efc7f5f8c85a94c
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-10-19 12:00:36 +0200

    MenuOptions Skapades för att visa menyn och välja mellan olika alternativen

--- AddressBook/Services/MenuManager.cs
+++ AddressBook/Services/MenuManager.cs
@@ -10,50 +10,72 @@ namespace AddressBook.Services
     // ett interface om de metoder som behövs till adressboken
     internal interface IMenuManager
     {
-        public void ShowMenuOptions();   
+        public void ShowMenuOptions();   // En metod för att visa menyn
         public void ShowAddressBook();
         public void ShowContactDetails(string id);
         public void ShowUpdateContact(Contact contact);
-        public void DeleteContact(string id);
+        public void DeleteContact(Guid id);
         public void ShowAddContact();
         public void ShowSettings();
 
     }
-    internal class MenuManager : IMenuManager
+    internal class MenuManager : IMenuManager  //Implementera interfacet
     {
-        public void DeleteContact(string id)
+        public void ShowMenuOptions()
         {
-            throw new NotImplementedException();
-        }
+            Console.Clear();
+            Console.WriteLine("¤¤¤¤¤¤ Menu ¤¤¤¤¤¤");
+            Console.WriteLine("1. View Address Book");
+            Console.WriteLine("2. Add New Contact");
+            Console.WriteLine("3. Settings");
+            Console.WriteLine();
+            Console.Write("Choose one option: ");
+            var option = Console.ReadLine();
 
-        public void ShowAddContact()
-        {
-            throw new NotImplementedException();
-        }
+            switch (option)
+            {
+                case "1":
+                    ShowMenuOptions();
+                    break;
+                                   
+                case "2":
+                    ShowAddContact();
+                    break;
+                
+                case "3":
+                    ShowSettings();
+                    break;
+                    
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
 
+            }
+
+        }
         public void ShowAddressBook()
         {
             throw new NotImplementedException();
         }
-
         public void ShowContactDetails(string id)
         {
             throw new NotImplementedException();
         }
-
-        public void ShowMenuOptions()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowSettings()
-        {
-            throw new NotImplementedException();
-        }
-
         public void ShowUpdateContact(Contact contact)
         {
             throw new NotImplementedException();
         }
+        public void DeleteContact(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public void ShowAddContact()
+        {
+            throw new NotImplementedException();
+        }      
+        public void ShowSettings()
+        {
+            throw new NotImplementedException();
+        }      
     }
 }
